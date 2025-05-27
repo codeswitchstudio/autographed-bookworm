8ac9476f755981cb92c61e95b5e905810c383edc
codeswitchstudio <toolbarista@gmail.com>
2025-05-27 10:35:35 +0100

    fixed panel. added more books

--- src/components/DetailPanel/styles.js
+++ src/components/DetailPanel/styles.js
@@ -47,7 +47,7 @@ export const Panel = styled.article`
 export const CloseWrapper = styled(Pill)`
   position: fixed;
   cursor: pointer;
-  top: 120px;
+  top: 40px;
   right: 40px;
   z-index: 4;
   display: ${({$state}) => ($state === 'entered' ? 'flex' : 'none')};
